2efe1106315e5d5d88970027e1b878bd7ec56b18
Nick Florin <nickmflorin@gmail.com>
2024-08-25 15:57:17 -0400

    Remove placeholder

--- src/app/(authenticated)/products/layout.tsx
+++ src/app/(authenticated)/products/layout.tsx
@@ -1,7 +1,5 @@
 import { type ReactNode } from "react";
 
-import { Module } from "~/components/structural/Module";
-
 interface ProductsLayoutProps {
   readonly table: ReactNode;
 }
@@ -9,9 +7,6 @@ interface ProductsLayoutProps {
 export default function ProductsLayout({ table }: ProductsLayoutProps) {
   return (
     <div className="flex flex-col gap-[16px]">
-      <div className="flex flex-row items-center">
-        <Module component="paper">Foo</Module>
-      </div>
       <div className="flex flex-row items-center grow min-h-[0px] overflow-auto">{table}</div>
     </div>
   );
